fb9aea3e8730fcbb53f7bd90d135bc6b84683679
DONG HUYNH <d01295306466@gmail.com>
2020-07-09 13:53:35 +0700

    k2

--- app/Http/Controllers/Api/v1/InvoiceController.php
+++ app/Http/Controllers/Api/v1/InvoiceController.php
@@ -9,6 +9,7 @@ use App\InvoiceProduct;
 use Illuminate\Http\Request;
 use App\Events\CustomerOrder;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class InvoiceController extends Controller
